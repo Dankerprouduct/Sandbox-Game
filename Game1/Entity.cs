cb80b286f4b66eb1f7fc22f0db0b24f77cbfcc82
David William Fitzgerald <Dankerprouduct@users.noreply.github.com>
2018-08-31 21:21:19 -0400

    initial

---
+++ Game1/Entity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics; 
+
+namespace Game1
+{
+    public class Entity
+    {
+        static int nextAvailibleID = 0; 
+        int id;
+
+        public void SetID(int value) => id = value;
+
+        public Entity()
+        {
+
+        }
+
+        public virtual void Update()
+        {
+              
+        }
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+
+    }
+}
